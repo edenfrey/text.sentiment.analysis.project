49dd0a25b9c53a8e1beda9a45f9a287a742f049d
Adrian Amir <aami0016@student.monash.edu>
2022-06-15 14:50:38 +0800

    ALTER Added Changes to <main.py> Imported libraries. Added starting code.

---
+++ main.py
@@ -0,0 +1,11 @@
+# Creator: Amir Adrian Amir Hamzah
+# Lisence: Apache Lisence, 2022
+
+# Importing Necessary Files
+import pickle
+import os 
+
+
+if __name__ == "__main__" :
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = dir_path + "\encoder.pkl"
