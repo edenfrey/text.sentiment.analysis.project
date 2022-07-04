5f42ffb63ea63dc69f633de515755c83a4440f2b
Amir Adrian Amir Hamzah <aami0016@student.monash.edu>
2022-07-04 10:28:29 +0800

    TIDY Tidied up some parts of the project directory.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# text.sentiment.analysis.project
+# Twitter Text Sentiment Analysis to Message Analysis Bot
+
 This is a trial project for SMS text analysis to predict mood.
+
+# NOTE
+
+Use GIT LFS to retrieve the large dataset.
+
+# Project Information
+
+- Self-Project
+- Creator: Amir Adrian Amir Hamzah
+- License: Apache License, 2022
--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 # Creator: Amir Adrian Amir Hamzah
-# Lisence: Apache Lisence, 2022
+# License: Apache License, 2022
 
 # Library Imports
 
-# DataFrame
-import pandas as pd
+import pandas as pd # DataFrame
 
-# Matplot
-import matplotlib as mpl
+import matplotlib as mpl    # Matplot
 import matplotlib.pyplot as plt
 
-# Scikit-learn
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split    # Scikit-learn
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.manifold import TSNE
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Keras
-from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer  # Keras
 from keras_preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Dropout, Embedding, Flatten, Conv1D, MaxPooling1D, LSTM
 from keras import utils
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 
-# nltk
-import nltk
+import nltk # nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
-# Word2vec
-import gensim
+import gensim   # Word2vec
 
-# Utility Lbraries
+# Utility Libraries
 import re
 import numpy as np
 import os
@@ -43,7 +37,7 @@ import time
 import pickle
 import itertools
 
-# Set log
+# Set Log
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # SETUP
@@ -52,8 +46,8 @@ DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
 DATASET_ENCODING = "ISO-8859-1"
 TRAIN_SIZE = 0.8
 
-# TEXT CLENAING
-TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+# TEXT CLEANING
+TEXT_CLEANING_REGEX = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
 # WORD2VEC 
 W2V_SIZE = 300
@@ -78,20 +72,18 @@ WORD2VEC_MODEL = "model.w2v"
 TOKENIZER_MODEL = "tokenizer.pkl"
 ENCODER_MODEL = "encoder.pkl"
 
-# Downloading Stopwords 
-
+# Downloading Stop-Words 
 nltk.download('stopwords')
 
 
 # SETUP
-
 # DATASET
 DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
 DATASET_ENCODING = "ISO-8859-1"
 TRAIN_SIZE = 0.8
 
-# TEXT CLENAING
-TEXT_CLEANING_RE =  "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+# TEXT CLEANING
+TEXT_CLEANING_REGEX =  "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
 # WORD2VEC 
 W2V_SIZE = 300
@@ -117,14 +109,10 @@ TOKENIZER_MODEL = "tokenizer.pkl"
 ENCODER_MODEL = "encoder.pkl"
 
 # Reading the File
-
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dataset_path = dir_path + "/input"
 dataset_filename = os.listdir(dataset_path)[0]
-
 dataset_file_path = os.path.join(dataset_path,dataset_filename)
-print("Open file:", dataset_file_path)
 df = pd.read_csv(dataset_file_path, encoding =DATASET_ENCODING , names=DATASET_COLUMNS)
 print("Dataset size:", len(df))
-print(df)
+print(df.head(10))
